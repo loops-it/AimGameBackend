789ecf5508d0d26963f6eaeb75b26eb6916ea81d
tj <thinupavani@gmail.com>
2024-01-01 08:44:29 +0530

    initial commit

---
+++ app/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+var jwt = require("jsonwebtoken");
+const { key } = require("../config");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("authorization");
+
+  if (!authHeader || authHeader.split(" ").length != 2) {
+    return res
+      .status(403)
+      .json({ success: false, code: 403, msg: "please provide token" });
+  }
+
+  try {
+    const token = authHeader.split(" ")[1];
+    var decoded = jwt.verify(token, key);
+    req.user = {
+      ...decoded.data,
+      role: ["testRole"],
+    };
+    next();
+  } catch (err) {
+    return res
+      .status(401)
+      .json({ success: false, code: 401, msg: "invalid token" });
+  }
+};
